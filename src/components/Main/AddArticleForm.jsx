fd6b53e8e41a673e776f1fcff72ddde487137907
federicoacker <acker.federico@gmail.com>
2026-05-04 15:01:51 +0200

    Iniziato a lavorare un po' di più alla form

--- src/components/Main/AddArticleForm.jsx
+++ src/components/Main/AddArticleForm.jsx
@@ -1,10 +1,24 @@
+import useState from "react";
 
-function AddArticleForm({articleList, setter}) {
-  return (
-    <form>
-        <input type="text"></input>
-    </form>
-  )
+const templateArticle =
+{
+    id: crypto.randomUUID(),
+    title: "Inserisci un titolo",
+    text: "Inserisci una descrizione",
+    image: "https://placehold.co/600x400"
+}
+
+
+function AddArticleForm({ articleList, setter }) {
+    const [newArticle, setNewArticle] = useState(templateArticle);
+    return (
+        <form>
+            <input type="text" value={newArticle.title} name="input-titolo" required/>
+            <input type="text" value={newArticle.text} name="input-text" required/>
+            <input type="file" value={newArticle.image} name="input-image"/>
+            <img src={newArticle.image}/>
+        </form>
+    )
 }
 
 export default AddArticleForm
