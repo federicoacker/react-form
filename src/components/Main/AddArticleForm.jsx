dd126c1bfaecc1891254e28d64dd63cad6ba323c
federicoacker <acker.federico@gmail.com>
2026-05-04 15:20:37 +0200

    Creato il change handler, che funziona per tutti i vari input, addirittura gli ho messo una condition per cui se usiamo l'upload per l'immagine lui si rende conto che è l'immagine che è di tipo file e semplicemente si prende l'URL dell'immagine. Adesso dovrò fare la funzione per il submit e mettere un submit button

--- src/components/Main/AddArticleForm.jsx
+++ src/components/Main/AddArticleForm.jsx
@@ -1,4 +1,4 @@
-import useState from "react";
+import { useState } from "react";
 
 const templateArticle =
 {
@@ -11,13 +11,41 @@ const templateArticle =
 
 function AddArticleForm({ articleList, setter }) {
     const [newArticle, setNewArticle] = useState(templateArticle);
+
+    const changeHandler = (event) => {
+        const inputField = event.target;
+        let submitValue = inputField.value;
+
+        if(inputField.name === "image"){
+            const filesArray = inputField.files;
+            if(filesArray.length === 1){
+                submitValue = URL.createObjectURL(filesArray[0]);
+            }
+        }
+        
+        const modifiedArticle = {
+            ...newArticle,
+            [inputField.name]:submitValue
+        }
+
+        setNewArticle(modifiedArticle);
+
+    }
+
     return (
-        <form>
-            <input type="text" value={newArticle.title} name="input-titolo" required/>
-            <input type="text" value={newArticle.text} name="input-text" required/>
-            <input type="file" value={newArticle.image} name="input-image"/>
-            <img src={newArticle.image}/>
+        <>
+        
+        <form className="form-control d-flex flex-column gap-2 py-3">
+            <h2 className="form-title">Aggiungi un nuovo Articolo!</h2>
+            <label for="title">Titolo dell'Articolo</label>
+            <input className="form-control" type="text" onChange={changeHandler} value={newArticle.title} name="title" required/>
+            <label for="text">Testo dell'Articolo</label>
+            <textarea className="form-control" onChange={changeHandler} value={newArticle.text} name="text" required/>
+            <label for="image">Immagine associata (opzionale)</label>
+            <input className="form-control" type="file" onChange={changeHandler} name="image"/>
+            <img className="img-fluid" src={newArticle.image}/>
         </form>
+        </>
     )
 }
 
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,16 +3,17 @@ import Row from "react-bootstrap/Row"
 import BlogCardList from "./BlogCardList.jsx"
 import articleList from "../../utils/data.js"
 import { useState } from "react"
+import AddArticleForm from "./AddArticleForm.jsx"
 
 function Main() {
     const [stateArticleList, setStateArticleList] = useState(articleList);
     return (
         <main className="bg-dark-subtle">
             <Container className="py-5">
-                <Row className="g-3 align-items-stretch">
+                <Row className="g-3 align-items-stretch my-3">
                     <BlogCardList articleList={stateArticleList} setter={setStateArticleList}/>
                 </Row>
-                
+                <AddArticleForm/>
             </Container>
         </main>
     )
