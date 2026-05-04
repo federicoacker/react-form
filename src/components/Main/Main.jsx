661a3b3840fab1a8a6ca789457ab86cd54747503
federicoacker <acker.federico@gmail.com>
2026-05-04 14:11:59 +0200

    Creato il main dove andrò a fare il display delle card

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,18 @@
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+
+function Main() {
+  return (
+    <main>
+        <Container>
+            <Row>
+                <Col xs={12} sm={12} md={12} lg={6} xl={4}>
+                </Col>
+            </Row>
+        </Container>
+    </main>
+  )
+}
+
+export default Main
